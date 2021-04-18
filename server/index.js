6f2ae1ee1704f4b6e7efab4518b22df49ec0ca4f
Treeboy <vinsanstavid@gmail.com>
2021-04-18 10:16:41 +0900

    fix index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,14 @@ const mongoose = require("mongoose");
 const config = require("./config");
 
 const connect = url => {
-  return mongoose.connect(url, config.db.options);
+  return mongoose
+      .connect(url, config.db.options)
+      .then(() => {
+        console.log("MongoDB is run successfully");
+      })
+      .catch(() => {
+        console.log("MongoDB failed");
+      });
 };
 
 if (require.main === module) {
