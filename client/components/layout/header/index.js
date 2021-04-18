33f7484ed252192ac76987356e455d78626d4706
sanvinvidsta <82734487+sanvinvidsta@users.noreply.github.com>
2021-04-19 03:28:34 +0530

    Update index.js

--- client/components/layout/header/index.js
+++ client/components/layout/header/index.js
@@ -46,7 +46,7 @@ const Header = ({ className, ...props }) => {
         <Button className={styles.logo} href="/">
           <Logo />
           <p>
-            <span>PRO - diaries </span> IIIT D&M
+            <span> PRO - diaries </span> IIITDM Kancheepuram 
           </p>
         </Button>
         <div style={{ flex: 1 }}></div>
